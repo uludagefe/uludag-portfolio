dd738e53ac7b58b17597ad497e3a50c3b32cc91d
Efe Uludağ <uludag.efe.tr@gmail.com>
2025-04-03 20:25:33 +0200

    Refactor next.config.ts to remove unused settings; add engines field in package.json for Node version

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ['https://uludag-portfolio.vercel.app/'], // Replace 'example.com' with your image domains if needed
-  },
-  output: 'standalone', // Ensures compatibility with Vercel's serverless environment
+  
 };
 
 export default nextConfig;
